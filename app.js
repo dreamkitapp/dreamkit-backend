7a634377a5eba257b17bd459608d4ab638fea7ef
Eden <edengorevoy@gmail.com>
2019-11-09 19:56:48 -0500

    mongodb changes

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require('express')
+const cors = require('cors') // added this
+const mongoose = require('mongoose');
+require('dotenv').config({ path: '.env' });
 const app = express()
 const port = 3000
 
+app.use(cors())
+app.use(express.json())
+
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true});
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully!")
+})
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
